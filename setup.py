5b6a82069fa4b941985cf8a9b72936fc63d88939
Florian HENRY <florian.henry@open-concept.pro>
2023-09-14 17:54:11 +0200

    day1

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
-from setuptools import setup, find_packages
+# TODO: Remove this file when  v15.0.0 is released
+from setuptools import setup
 
-with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
+name = "airplane_mode"
 
-# get version from __version__ variable in airplane_mode/__init__.py
-from airplane_mode import __version__ as version
-
-setup(
-	name="airplane_mode",
-	version=version,
-	description="Frappe Certification Process buildwithhussain",
-	author="scopen.fr",
-	author_email="florian.henry@scopen.fr",
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
-)
+setup()
