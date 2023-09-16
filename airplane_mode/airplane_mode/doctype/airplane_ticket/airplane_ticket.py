2f95409632ca9ec23e4132ea15e7b28b074ad833
Florian HENRY <florian.henry@open-concept.pro>
2023-09-16 12:10:58 +0200

    show-me

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,12 +1,18 @@
 # Copyright (c) 2023, scopen.fr and contributors
 # For license information, please see license.txt
 
+from random import choice, randint
+
 import frappe
 from frappe import _
 from frappe.model.document import Document
 
 
 class AirplaneTicket(Document):
+	def before_insert(self):
+		# <random-integer><random-capital-alphabet-from-A-to-E>
+		self.seat = str(randint(1, 99)) + choice(["A", "B", "C", "D", "E", "F"])
+
 	def validate(self):
 		self.total_amount = 0
 		extisting_addon_type = []
