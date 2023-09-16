ac995de03e92a50e76a9496926cef0b0e6d91594
Florian HENRY <florian.henry@open-concept.pro>
2023-09-16 14:05:42 +0200

    web site publish

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -1,10 +1,9 @@
 # Copyright (c) 2023, scopen.fr and contributors
 # For license information, please see license.txt
 
-# import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
 
-class AirplaneFlight(Document):
+class AirplaneFlight(WebsiteGenerator):
 	def on_submit(self):
 		self.status = "Complete"
