dbe86de31e76f7cd880ece4b7999df223c0e63bd
Florian HENRY <florian.henry@open-concept.pro>
2023-09-16 15:48:22 +0200

    refactor: fix result

--- airplane_mode/airplane_mode/doctype/flight_passenger/flight_passenger.py
+++ airplane_mode/airplane_mode/doctype/flight_passenger/flight_passenger.py
@@ -9,4 +9,4 @@ class FlightPassenger(Document):
 	def validate(self):
 		self.full_name = str(self.first_name)
 		if self.last_name:
-			self.full_name = " " + str(self.last_name)
+			self.full_name += " " + str(self.last_name)
