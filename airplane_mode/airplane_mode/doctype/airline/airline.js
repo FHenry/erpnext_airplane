12183fb8ac3ad5fc80c2c301f336f155629ead9b
Florian HENRY <florian.henry@open-concept.pro>
2023-09-16 16:46:56 +0200

    web site link

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Airline', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+    if (frm.doc.website) {
+      let elmt = $(frm.page.sidebar).find('div.form-sidebar').first();
+      let a = $('<a>').prop("href",frm.doc.website).text('View Website').css('font-weight','bold');
+      let div = $('<div>').addClass('pb-3').append(a);
+      elmt.prepend(div);
+    }
 
-	// }
+	}
 });
