542fee2f3d2c6b7fe6c9dee835b32f34e2d07b58
Florian HENRY <florian.henry@open-concept.pro>
2023-09-18 16:37:24 +0200

    perms in doctype

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -4,10 +4,11 @@
 frappe.ui.form.on('Airline', {
 	refresh: function(frm) {
     if (frm.doc.website) {
-      let elmt = $(frm.page.sidebar).find('div.form-sidebar').first();
-      let a = $('<a>').prop("href",frm.doc.website).text('View Website').css('font-weight','bold');
-      let div = $('<div>').addClass('pb-3').append(a);
-      elmt.prepend(div);
+      // let elmt = $(frm.page.sidebar).find('div.form-sidebar').first();
+      // let a = $('<a>').prop("href",frm.doc.website).text('View Website').css('font-weight','bold');
+      // let div = $('<div>').addClass('pb-3').append(a);
+      // elmt.prepend(div);
+      frm.add_web_link(frm.doc.website);
     }
 	}
 });
